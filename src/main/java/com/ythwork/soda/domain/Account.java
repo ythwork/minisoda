1d7780a68e4f40cce1743e210ed6f790ba902a7c
ythwork <ythwork83@gmail.com>
2020-10-29 12:17:17 +0900

    schema

--- src/main/java/com/ythwork/soda/domain/Account.java
+++ src/main/java/com/ythwork/soda/domain/Account.java
@@ -8,6 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -31,7 +32,7 @@ public class Account {
 	private Member member;
 	
 	// 테이블 구성 시 ON DELETE CASCADE 명시
-	@ManyToOne(fetch=FetchType.LAZY)
+	@OneToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name = "openapi_id")
 	private Openapi openapi;
 	
