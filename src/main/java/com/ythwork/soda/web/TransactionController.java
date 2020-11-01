186a0dbbc533d878685ddf483a1c439609aec416
ythwork <ythwork83@gmail.com>
2020-11-02 07:00:20 +0900

    web layer - transaction controller

--- src/main/java/com/ythwork/soda/web/TransactionController.java
+++ src/main/java/com/ythwork/soda/web/TransactionController.java
@@ -9,9 +9,11 @@ import java.util.stream.Collectors;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -71,4 +73,13 @@ public class TransactionController {
 				.body(assembler.toModel(transaction));
 	}
 	
+	@PutMapping("/{id}/complete")
+	public ResponseEntity<?> complete(@PathVariable Long id) {
+		return null;
+	}
+	
+	@DeleteMapping("/{id}/cancel")
+	public ResponseEntity<?> cancel(@PathVariable Long id) {
+		return null;
+	}
 }
